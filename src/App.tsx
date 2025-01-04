44298dc4f4f636ed6c80ac0cc25d8425e7a89996
Sabrina Ferreira <saaaferreira6@gmail.com>
2025-01-04 20:07:27 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 
 import './App.css'
-import MainContent from './components/mainContent'
+import AboutContainer from './components/AboutContainer'
+import InformationContainer from './components/InformationContainer'
+import MainContent from './components/MainContent'
+import ProjectsContainer from './components/ProjectsContainer'
 import Sidebar from './components/Sidebar'
+import SocialNetworks from './components/SocialNetworks'
+import TechnologiesContainer from './components/TechnologiesContainer'
 
 const App = () => {
 
@@ -11,6 +16,11 @@ const App = () => {
         <h1>Sabrina Ferreira</h1>
         <Sidebar />
         <MainContent />
+        <AboutContainer />
+        <InformationContainer />
+        <ProjectsContainer />
+        <SocialNetworks />
+        <TechnologiesContainer />
       </div>
     
   )

bdc7a9e3761e57758ce0245cb899c7721f5871b1
Sabrina Ferreira <saaaferreira6@gmail.com>
2025-01-08 20:08:30 -0300

    text update

--- src/components/ProjectsContainer.tsx
+++ src/components/ProjectsContainer.tsx
@@ -4,7 +4,7 @@ const ProjectsContainer = () =>{
     return(
        <section className='projects-container'>
             <h2>Projects</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni cumque adipisci accusantium in quis excepturi illum enim cupiditate, sunt reiciendis iusto tenetur soluta quam pariatur, et atque ipsa porro? Voluptatem!</p>
+            <p>Confira meus projetos:</p>
             <a href='https://github.com/SabrinaDev0621?tab=repositories'target='_blank' className='btn'>Ver Projetos</a>
        </section>
     )
